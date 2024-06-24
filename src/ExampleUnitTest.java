01ec85380da0de24c9c376f9bd1c6ae4ce774b54
heilviva <145266521+heilviva@users.noreply.github.com>
2024-06-24 04:59:12 +0300

    Update ExampleUnitTest.java

--- src/ExampleUnitTest.java
+++ src/ExampleUnitTest.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class ExampleUnitTest {
+package com.example.webview;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
 }
